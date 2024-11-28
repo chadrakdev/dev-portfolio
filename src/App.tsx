272ff26b85880b0a4cefeb354dd50945f0935d6e
chadrakdev <chadrak@outlook.com>
2024-11-28 13:19:13 +0000

    Import pages to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NavBar from './components/navigation/NavBar'
+import Home from './components/pages/Home'
+import Work from './components/pages/Work'
+import Projects from './components/pages/Projects'
+import Blog from './components/pages/Blog'
 
 const App = () => {
 
