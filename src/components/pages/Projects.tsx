aa75bbb9415975b03052561f609443387ff8cc78
chadrakdev <chadrak@outlook.com>
2024-12-02 12:38:08 +0000

    Add project data mapping in Projects

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -1,9 +1,63 @@
+import { styled, Box, Typography, List, ListItem } from "@mui/material"
+import { projects } from "../../data/projects.data"
+
+const StyledSection = styled(Box)(() => ({
+	paddingBottom: '3rem'
+}))
+
+const StyledHeading = styled(Typography)(() => ({
+	fontWeight: 'bold',
+}))
+
+const StyledText = styled(Typography)(() => ({
+	fontWeight: 'normal',
+	paddingBottom: '1rem'
+}))
+
+const StyledList = styled(List)(() => ({
+	display: 'flex',
+	flexDirection: 'row',
+	justifyContent: 'start',
+	padding: '0.3rem 0'
+}))
+
+const StyledListItem = styled(ListItem)(({ theme }) => ({
+	fontWeight: 'normal',
+	fontSize: '0.8rem',
+	color: theme.palette.text.secondary,
+	border: '0.1rem solid',
+	borderRadius: '0.5rem',
+	margin: '0.3rem 0.3rem 0.3rem 0',
+	padding: '0.3rem',
+	width: 'auto',
+	'&:hover': {
+		color: theme.palette.text.primary,
+		backgroundColor: theme.palette.text.secondary
+	}
+}))
+
+const StyledItemWrapper = styled(Box)(() => ({
+	paddingBottom: '2rem'
+}))
 
 const Projects = () => {
 	return (
-		<div>
-      
-		</div>
+		<StyledSection key="projects">
+			<StyledHeading sx={{ paddingBottom: '1rem' }}>Projects</StyledHeading>
+			{projects.map(data =>
+				<StyledItemWrapper key={data.id}>
+					<StyledHeading>{data.title}</StyledHeading>
+					{data.tags.length > 0 && (
+						<StyledList disablePadding>
+							{data.tags.map(tag => (
+								<StyledListItem key={tag} disableGutters>{tag}</StyledListItem>
+							))}
+						</StyledList>
+					)}
+					<StyledText>{data.description}</StyledText>
+				</StyledItemWrapper>
+			)}
+		</StyledSection>
 	)
 }
 
