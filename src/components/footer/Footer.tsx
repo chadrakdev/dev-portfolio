423eddf26db0a9414abb3b165c9b022f25dde011
chadrakdev <chadrak@outlook.com>
2024-12-12 18:25:20 +0000

    Remove carried over styling in Footer component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,18 +1,8 @@
 import { styled, Container } from "@mui/material"
 import { Text } from "../styled/StyledText"
 
-const StyledFooter = styled(Container)(({ theme }) => ({
-	display: 'flex',
-	flexDirection: 'row',
-	justifyContent: 'space-between',
-	verticalAlign: 'bottom',
-	padding: '0.7rem 0',
-	backgroundColor: theme.palette.navBackground,
-	backgroundImage: 'none',
-	backdropFilter: 'blur(0.1rem)',
-	boxShadow: 'none',
-	top: '0',
-	position: 'sticky'
+const StyledFooter = styled(Container)(() => ({
+	padding: '0'
 }))
 
 const Footer = () => {
