947a588e5cf57c44f636003836b6ac50338b06ea
chadrakdev <chadrak@outlook.com>
2024-12-18 07:36:00 +0000

    Update projects data

--- src/data/projects.data.ts
+++ src/data/projects.data.ts
@@ -3,23 +3,30 @@ import { ProjectHistory } from "../types/projectHistory";
 export const projects: ProjectHistory[] = [
 	{
 		id: 1,
-		title: "Game Library API",
-		url: "https://github.com/chadrakdev/GameLibraryAPI",
+		title: "Password Generator API",
+		url: "https://github.com/chadrakdev/PasswordGeneratorAPI",
 		tags: ["C#", ".NET", "MVC"],
-		description: "A simple API for managing a game library with CRUD operations to add, update, and remove games in the database."
+		description: "API for generating customisable, secure passwords focusing on security, encryption, and the algorithmic side of API development."
 	},
 	{
 		id: 2,
-		title: "Record Shop API",
-		url: "https://github.com/chadrakdev/RecordShopAPI",
-		tags: ["Java", "Spring Boot", "SQL"],
-		description: "A REST API for managing stock data in a record shop, using an in-memory database for quick updates and queries."
+		title: "Portfolio Website",
+		url: "https://github.com/chadrakdev/dev-portfolio",
+		tags: ["TypeScript", "React"],
+		description: "Minimalist portfolio demonstrating ability to design a responsive, modern user interface and work with TypeScript for type safety and create dynamic web components."
 	},
 	{
 		id: 3,
-		title: "Mars Rover",
-		url: "https://github.com/chadrakdev/MarsRover",
-		tags: ["Java"],
-		description: "A console app simulating a rover navigating Mars plateaus to collect research samples, built with Java."
+		title: "Game Library API",
+		url: "https://github.com/chadrakdev/GameLibraryAPI",
+		tags: ["C#", ".NET", "MVC"],
+		description: "RESTful API for managing game collections with full CRUD operations showcasing the ability to manage data models, implement CRUD operations, and potential for database integration."
+	},
+	{
+		id: 4,
+		title: "Record Shop API",
+		url: "https://github.com/chadrakdev/RecordShopAPI",
+		tags: ["Java", "Spring Boot", "SQL"],
+		description: "RESTful API for managing a record store's inventory with in-memory data storage with emphasis on scalability and full CRUD operations."
 	}
 ];
