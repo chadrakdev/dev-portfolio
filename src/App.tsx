62ac0ac8eb48bc10ff3f9f3f4841d7cdd7af56cf
chadrakdev <chadrak@outlook.com>
2024-12-18 06:51:17 +0000

    Add dynamic document title logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { darkTheme, lightTheme } from './theme'
 import { ThemeProvider } from '@mui/material'
 import CssBaseline from '@mui/material/CssBaseline'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import AppLayout from './components/layouts/AppLayout'
 import PageLayout from './components/layouts/PageLayout'
 import NavBar from './components/navigation/NavBar'
@@ -19,23 +19,55 @@ const App = () => {
 		setIsDarkMode((prevMode) => !(prevMode))
 	}
 
+	const location = useLocation();
+
+	const parsePath = (pathname: string) => {
+		switch (pathname) {
+		case ("/"): {
+			return "Home"
+		}
+		case ("/work"): {
+			return "Work"
+		}
+		case ("/projects"): {
+			return "Projects"
+		}
+		case ("/blog"): {
+			return "Blog"
+		}
+		default: {
+			return ""
+		}
+		}
+	}
+
+	const setDocumentTitle = () => {
+		const title = parsePath(location.pathname)
+
+		if (title.length > 1) {
+			document.title = `${parsePath(location.pathname)} | Chadrak H`
+		}
+	}
+	
+	useEffect(() => {
+		setDocumentTitle();
+	}, [location.pathname])
+
 	return (
 		<ThemeProvider theme={isDarkMode? darkTheme : lightTheme}>
 			<CssBaseline />
-			<Router>
-				<AppLayout>
-					<NavBar onToggleTheme={toggleTheme} isDarkMode={isDarkMode} />
-					<PageLayout>
-						<Routes>
-							<Route path="/" element={<Home />} />
-							<Route path="/work" element={<Work />} />
-							<Route path="/projects" element={<Projects />} />
-							<Route path="/blog" element={<Blog />} />
-						</Routes>
-					</PageLayout>
-					<Footer />
-				</AppLayout>
-			</Router>
+			<AppLayout>
+				<NavBar onToggleTheme={toggleTheme} isDarkMode={isDarkMode} />
+				<PageLayout>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/work" element={<Work />} />
+						<Route path="/projects" element={<Projects />} />
+						<Route path="/blog" element={<Blog />} />
+					</Routes>
+				</PageLayout>
+				<Footer />
+			</AppLayout>
 		</ThemeProvider>
 	)
 }
