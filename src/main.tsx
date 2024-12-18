892cad876e2454f4645eb3e2dab6de16c30dd269
chadrakdev <chadrak@outlook.com>
2024-12-18 06:50:58 +0000

    Wrap App in Router context

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react'
+import { BrowserRouter as Router } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
@@ -9,6 +10,8 @@ import '@fontsource/roboto/700.css';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		<Router>
+			<App />
+		</Router>
 	</StrictMode>,
 )
