eb305b9252580be0ae8ef6f3117db079ec386e7d
chadrakdev <chadrak@outlook.com>
2024-12-24 14:31:31 +0000

    Update icon sizes

--- src/components/styled/StyledIcons.ts
+++ src/components/styled/StyledIcons.ts
@@ -2,9 +2,11 @@ import { styled } from "@mui/material";
 import { LightModeOutlined, DarkModeOutlined } from "@mui/icons-material";
 
 export const LightMode = styled(LightModeOutlined)(({ theme }) => ({
-	color: theme.palette.text.primary
+	color: theme.palette.text.primary,
+	fontSize: "18px"
 }))
 
 export const DarkMode = styled(DarkModeOutlined)(({ theme }) => ({
-	color: theme.palette.text.primary
+	color: theme.palette.text.primary,
+	fontSize: "18px"
 }))
