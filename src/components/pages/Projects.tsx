925f27bc95290848ad0065dc8b05d989308c17be
chadrakdev <chadrak@outlook.com>
2024-12-24 11:53:06 +0000

    Restructure Projects page

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -1,29 +1,25 @@
-import { Heading, Subhead, Text } from "../styled/StyledText"
+import { Heading } from "../styled/StyledText"
 import { projects } from "../../data/projects.data"
-import { PageSection, ContentSection } from "../styled/StyledContainers"
+import { PageSection } from "../styled/StyledContainers"
 import { AnimatedContainer } from "../styled/StyledContainers"
+import ContentList from "../content/ContentList"
+import ContentListItem from "../content/ContentListItem"
 
 const Projects = () => {
 	return (
 		<AnimatedContainer>
-			<PageSection key="projects">
+			<PageSection>
 				<Heading hasPadding>Projects</Heading>
-				{projects.map(data =>
-					<ContentSection key={data.id}>
-						<Heading>{data.title}</Heading>
-						<Subhead>
-  								Technologies: {" "}
-							{data.tags
-								.map((tag, index, array) => (
-									<span key={index}>
-										{tag}
-										{index < array.length - 1 && ", "}
-									</span>
-								))}
-						</Subhead>
-						<Text>{data.description}</Text>
-					</ContentSection>
-				)}
+				<ContentList>
+					{projects.map((data) => (
+						<ContentListItem
+							key={data.id}
+							title={data.title}
+							description={data.description}
+							url={data.url}
+						/>
+					))}
+				</ContentList>
 			</PageSection>
 		</AnimatedContainer>
 	)
