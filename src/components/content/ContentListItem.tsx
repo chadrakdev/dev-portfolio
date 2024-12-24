cca4dcab243ea7ce3caf6db0775d026103c427de
chadrakdev <chadrak@outlook.com>
2024-12-24 11:44:53 +0000

    Create ContentList and ContentListItem components

---
+++ src/components/content/ContentListItem.tsx
@@ -0,0 +1,30 @@
+import { ContentSection } from "../styled/StyledContainers";
+import { Heading, Text } from "../styled/StyledText";
+import { LinkWrapper } from "../styled/StyledLinks";
+
+interface ContentListItemProps {
+    title: string;
+    description: string;
+    url: string;
+}
+
+const ContentListItem: React.FC<ContentListItemProps> = ({ title, description, url }) => {
+	return (
+		<ContentSection
+			hasBorder
+			hasMargin
+			hasPadding
+			enableHover
+		>
+			<LinkWrapper
+				target="_blank"
+				rel="noopener"
+				to={url}>
+				<Heading>{title}</Heading>
+				<Text>{description}</Text>
+			</LinkWrapper>
+		</ContentSection>
+	)
+}
+
+export default ContentListItem
