733614ca6cf5033fcdc8e9dda304b5a537b0b4bb
chadrakdev <chadrak@outlook.com>
2024-12-28 16:27:35 +0000

    Implement NavContainer in NavBar

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -1,22 +1,8 @@
-import { styled } from "@mui/system";
-import { AppBar, IconButton } from "@mui/material"
+import { IconButton } from "@mui/material"
 import { Code } from "@mui/icons-material";
 import { LightMode, DarkMode } from "../styled/StyledIcons";
 import { LinkIcon, LinkText } from "../styled/StyledLinks";
-
-const StyledNav = styled(AppBar)(({ theme }) => ({
-	display: 'flex',
-	flexDirection: 'row',
-	justifyContent: 'space-between',
-	verticalAlign: 'bottom',
-	padding: '0.7rem 0',
-	backgroundColor: theme.palette.navBackground,
-	backgroundImage: 'none',
-	backdropFilter: 'blur(0.1rem)',
-	boxShadow: 'none',
-	top: '0',
-	position: 'sticky'
-}))
+import { NavContainer, ContentSection } from "../styled/StyledContainers";
 
 interface NavBarProps {
 	onToggleTheme: () => void;
@@ -25,17 +11,17 @@ interface NavBarProps {
 
 const NavBar: React.FC<NavBarProps> = ({ onToggleTheme, isDarkMode }) => {
 	return (
-		<StyledNav>
-			<LinkIcon to="/"><Code /></LinkIcon>
-			<div>
-				<LinkText to="work">work</LinkText>
-				<LinkText to="projects">projects</LinkText>
-				<LinkText to="blog">blog</LinkText>
+		<NavContainer>
+			<LinkIcon hasPadding to="/"><Code /></LinkIcon>
+			<ContentSection>
+				<LinkText hasPadding to="work">work</LinkText>
+				<LinkText hasPadding to="projects">projects</LinkText>
+				<LinkText hasPadding to="contact">contact</LinkText>
 				<IconButton onClick={onToggleTheme}>
 					{isDarkMode ? <DarkMode /> : <LightMode />}
 				</IconButton>
-			</div>
-		</StyledNav>
+			</ContentSection>
+		</NavContainer>
 	);
 };
 
